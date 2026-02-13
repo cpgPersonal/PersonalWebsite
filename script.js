10f134e1047854400c2e5ac2c73760499921fc4b
Callum Piper-Gilbert <gh020321>
2026-02-13 22:01:24 +0000

    Added more style

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-function type(text, id) {
+//The different functions
+
+//Function for typing text
+function type(text, id, speed) {
     let index = 0;
     const element = document.getElementById(id);
     function typing() {
         element.innerText = text.slice(0, index);
         index++;
         if (index <= text.length) {
-            setTimeout(typing, 100);
+            setTimeout(typing, speed); //Runs this function again with a sleep timer before it gets run
         }
     }
     typing();
 }
-type("Callum Piper-Gilbert", "mainName");
-type("University of Reading", "mainUni");
-type("BSc Computer Science", "mainCourse");
+
+//Function for parts of website appearing 
+function slide() {
+    
+}
+
+
+//Running the functions
+type("Callum Piper-Gilbert", "mainName", 100);
+type("University of Reading", "mainUni", 100);
+type("BSc Computer Science", "mainCourse", 100);
+
+
+
+const slides = document.querySelectorAll('.slide'); //Selects all that have .slide as the style
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show'); // adds when shown
+    } else {
+      entry.target.classList.remove('show'); // removes when scrolling up
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+slides.forEach(slide => {
+  observer.observe(slide);
+});
